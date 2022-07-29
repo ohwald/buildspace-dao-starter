b93dbfd9245dcd64705b5288e505739ee37f7664
yangchao <yangchao@migu.cn>
2022-07-29 09:50:28 +0800

    feat: modify content

--- src/App.jsx
+++ src/App.jsx
@@ -372,7 +372,7 @@ const App = () => {
   // Render mint nft screen.
   return (
     <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <h1>Mint your free Sport DAO Membership NFT</h1>
       <button disabled={isClaiming} onClick={mintNft}>
         {isClaiming ? 'Minting...' : 'Mint your nft (FREE)'}
       </button>
